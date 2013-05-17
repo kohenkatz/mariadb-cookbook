3ab27f046e91f783ea098a4f718ede4a016269ea
Marcin Koziuk <marcin.koziuk@gmail.com>
2013-05-17 22:52:04 +0200

    Don't use upstart

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,5 +26,8 @@
 
 default['mariadb']['version'] = "5.5"
 
+# MariaDB doesn't use upstart like vanilla MySQL does.
+normal['mysql']['use_upstart'] = false
+
 normal['mysql']['client']['packages'] = %W{mariadb-client}
 normal['mysql']['server']['packages'] = %W{mariadb-server}
