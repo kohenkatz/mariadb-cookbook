cef9ee3d8dd4fe4ed3d5ab86fe146654ff4cd48a
Adrien Ferré <ferre.adrien@gmail.com>
2013-08-25 23:43:37 +0200

    Added check for install_oqgraph node variable

--- recipes/server.rb
+++ recipes/server.rb
@@ -27,8 +27,19 @@
 include_recipe "mariadb::mariadb_repo"
 include_recipe "mysql::server"
 
+if node['mariadb']['oqgraph']
+   
+cookbook_file node['mariadb']['oqgraph']['install_file'] do
+  source "install_oqgraph.sql"
+  owner "root"
+  group "root"
+  mode "0600"
+end
+
 execute "mariadb-install-oqgraph" do
    command "mysql -u #{node['mysql']['username']} -p#{node['mysql']['server_root_password']} < #{node['mariadb']['oqgraph']['install_file']}"
    action :run
    only_if { `/usr/bin/mysql -u #{node['mysql']['username']} -p#{node['mysql']['server_root_password']} -D mysql -r -B -N -e \"SELECT count(*) from information_schema.ENGINES e where e.ENGINE = 'OQGRAPH'"`.to_i == 0 }
+end
+
 end
