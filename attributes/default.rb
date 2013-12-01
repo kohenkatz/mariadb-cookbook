f1b8dc848ed8b79154779ba2061e738ba1ceeb2b
Ptah Dunbar <yarr@piratedunbar.com>
2013-11-30 20:04:09 -0500

    File cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,12 +24,12 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-default['mariadb']['version'] = "5.5"
-default['mariadb']['oqgraph'] = false
-default['mariadb']['oqgraph_install_file']= "/tmp/install_oqgraph.sql"
+default[:mariadb][:version] = "5.5"
+default[:mariadb][:oqgraph] = false
+default[:mariadb][:oqgraph_install_file]= "/tmp/install_oqgraph.sql"
 
 # MariaDB doesn't use upstart like vanilla MySQL does.
-normal['mysql']['use_upstart'] = false
+normal[:mysql][:use_upstart] = false
 
-normal['mysql']['client']['packages'] = %W{mariadb-client libmariadbclient-dev}
-normal['mysql']['server']['packages'] = %W{mariadb-server}
+normal[:mysql][:client][:packages] = %W{mariadb-client libmariadbclient-dev}
+normal[:mysql][:server][:packages] = %W{mariadb-server}
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,5 @@ description      "Installs/Configures MariaDB"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends          "mysql", ">= 1.3.0"
+depends          "mysql"
 depends          "apt"
-
-suggests         "apt"
