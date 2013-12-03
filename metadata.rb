b50d0ff02112b84665902880a1c34ec1ec31ddcc
Pirate Dunbar <yarr@piratedunbar.com>
2013-12-03 15:22:45 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "marcin.koziuk@gmail.com"
 license          "MIT"
 description      "Installs/Configures MariaDB"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.8"
 
 depends          "mysql"
 depends          "apt"
